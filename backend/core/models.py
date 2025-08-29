cf71aec2d3fadb4d734d104caf5612d36fe1bf23
marcus <chessman.it023@gmail.com>
2025-08-29 15:13:01 +0800

    Connent to MySQL database

--- backend/core/models.py
+++ backend/core/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,25 +10,46 @@ class User(models.Model):
         return self.username
 
 class Customer(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+class MembershipLevel(models.Model):
+    class Level(models.IntegerChoices):
+        FREE = 0, '免費會員'
+        SILVER = 1, '銀級會員'
+        GOLD = 2, '金級會員'
+        PLATINUM = 3, '白金會員'
+    id = models.IntegerField(primary_key=True, choices=Level.choices)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.get_id_display()
 
 class Merchant(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    membership_level = models.ForeignKey(MembershipLevel, on_delete=models.PROTECT)
 
-# Restaurant model
 class Restaurant(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    body = models.TextField()
-    status = models.CharField(max_length=50)
+    introduction = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
-    
+
+class Comment(models.Model):
+    comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+class Service(models.Model):
+    description = models.TextField()
 
 class Order(models.Model):
-    id = models.AutoField(primary_key=True)
+    merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE)
+    payment_method = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=1)
+    status = models.CharField(max_length=50)
+    issued_time = models.DateTimeField(auto_now_add=True)
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True, blank=True)
--- backend/project01/settings.py
+++ backend/project01/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -72,10 +73,21 @@ WSGI_APPLICATION = 'project01.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project01_db',
+        'USER': 'root',
+        'PASSWORD': 'chessmanonline',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',  
     }
 }
 
